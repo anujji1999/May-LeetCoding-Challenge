bfd934ffdd70225a489f4406de82d1a9bb499349
Anuj Gupta <anujji1999@gmail.com>
2020-05-09 09:16:30 +0530

    Added Code for Day 8

---
+++ Day 8/CheckIfStraightLine.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkStraightLine(int[][] coordinates) {
+        float slope = (float)(coordinates[1][1] - coordinates[0][1]) / (float)(coordinates[1][0] - coordinates[0][0]);
+
+        for(int i=2; i<coordinates.length; i++){
+            float temp = (float)(coordinates[i][1] - coordinates[i-1][1]) / (float)(coordinates[i][0] - coordinates[i-1][0]);
+            if(temp != slope){
+                return false;
+            }
+        }
+        return true;
+    }
+}
